5c9649a2f33790241721b9b28126712f647fa912
rajan.jedi <rajan.jedi@gmail.com>
2015-12-08 18:17:47 -0500

    changed table where test to use auto for loop

--- datatable/test/data_table_expr_tests.cpp
+++ datatable/test/data_table_expr_tests.cpp
@@ -14,16 +14,16 @@ TEST(data_table_expr_tests, simple)
 	// expression to search for
 	string expr = "age = 43 and (income > 0 and income <= 1000000)";
 
-	int num_rows_found = 0;
+	auto num_rows_found = 0;
 	// get rows that match expression
-	for (auto r = table.where(expr).begin(); r != table.where(expr).end(); ++r )
+	for (auto r : table.where(expr))
 	{
 		num_rows_found++;
 
-		int32_t age = r->get<int32_t>("age");
+		int32_t age = r.get<int32_t>("age");
 		ASSERT_EQ(age, 43);
 
-		int64_t income = r->get<int64_t>("income");
+		int64_t income = r.get<int64_t>("income");
 		ASSERT_TRUE(income > 0);
 		ASSERT_TRUE(income <= 1000000);
 	}

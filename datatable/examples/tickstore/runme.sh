044feb0d07e4b3daaedca8ded7a833db765764d8
James Longo <jlongo@STRATUS.jlongo.org>
2016-04-25 21:38:49 -0400

    updated tickstore example documentation and added runme.sh

---
+++ datatable/examples/tickstore/runme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tick_file="./eurusd.ticks"
+
+if [ -n "$1" ]; then
+	tick_file=$1
+else
+	echo "no file specified.  generating $tick_file"	
+	./tickgen.py > $tick_file
+fi
+
+if [ -f "$tick_file" ]; then
+	echo "running tickstore..."
+	#TODO: use the correct path for the tickstore binary
+	./tickstore $tick_file
+else
+	echo "$tick_file does not exist"
+fi
